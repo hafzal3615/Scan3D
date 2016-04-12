b95fb542172ba63a9b70b503d79b3669c6250be8
Jay Taves <jht99@cornell.edu>
2016-04-12 16:33:22 +0200

    Cleanup of bad folder
    -- I guess that fixed it

--- DAVID_SDK_1.8.2/davidSDK_Examples/SimpleCalibration.cpp
+++ DAVID_SDK_1.8.2/davidSDK_Examples/SimpleCalibration.cpp
@@ -20,34 +20,20 @@ void main_SimpleCalibration()
 		// david::Client::Disconnect is automatically called in destructor of david::Client.
 		david::Client david;
 		david.Connect();
-		david.mainWindow().Show();
 
-		/*
-				std::vector<std::string> names = david.sls().GetAvailableCameraNames();
-				for (int i = 0; i < names.size(); i++) {
-					std::string name = names[i];
-						printf("%s", name);
-				}
-		*/
-		// Select screen and camera.
-		david.sls().SetScreenID(2);
-		david.sls().SelectCamera("Logitech HD Pro Webcam C920 (4076D97F)");
-		/*
-		
-		JAY FUCKING SUCKS
-		
-		
-		*/
+		david.sls().SetScreenID(0);
+		david.sls().SelectCamera("Logitech HD Pro Webcam C920 (4076D97F)"); 
+
 		// Tells DAVID server to calibrate, and save calibration parameters to .cal files.
+		david.sls().Calibrate(120.0);
+		david.sls().ExportCalibration("camera.cal", "projector.cal");
 
-		david.sls().Calibrate(150.0);
-		//david.sls().ExportCalibration("camera.cal", "projector.cal");
 		/*
 		david.sls().SetCameraProperty("Gain", 0);
 		david.sls().SetCameraProperty("Exposure", 0);
 		david.sls().SetCameraProperty("Brightness", 0);
 		david.sls().SetCameraProperty("Contrast", 0);
-
+		
 		int gain = david.sls().GetCameraProperty("Gain", david::CamPropsForScanning);
 		int contrast = david.sls().GetCameraProperty("Contrast", david::CamPropsForScanning);
 		int brightness = david.sls().GetCameraProperty("Brightness", david::CamPropsForScanning);
@@ -59,24 +45,32 @@ void main_SimpleCalibration()
 		printf("Exposure: %d\n", exposure);
 		*/
 
+		time_t begin = time(0);
+		std::string filename;
+		{
+			tm tstruct;
+			localtime_s(&tstruct, &begin);
+			char buf[80];
+			strftime(buf, sizeof(buf), "%Y-%m-%d_%H-%M-%S", &tstruct);	
+
+			filename = buf;
+			filename += ".obj";
+
+			std::cout << "filename: " << filename << std::endl;
+		}
+		
+		// Scan and export.
+		david.sls().Scan();
+
+		david.sls().ExportMesh(filename);
+
 		// Re-import calibration (this step is not necessary here, it is just for demonstration/testing):
 		//david.sls().ImportCalibration("camera.cal", "projector.cal");
-
 		
 		// david::Client::Disconnect is automatically called in destructor of david::Client
 		// at the end of this { } block
-
-		for (int i = 0; i < 10; i++) {
-			Sleep(1000);
-			printf("%d", 10 - i);
-		}
-
-		int num = david.sls().Scan();
-		std::cout << "New scan consists of " << num << " 3D points\n";
-
-		david.sls().ExportMesh("scan.obj");
 	}
-	catch (david::Exception& e) 
+	catch (david::Exception& e)
 	{
 		e.PrintError();
 	}
--- DAVID_SDK_1.8.2/davidSDK_Examples/main.cpp
+++ DAVID_SDK_1.8.2/davidSDK_Examples/main.cpp
@@ -14,7 +14,7 @@ void main()
 	examples::main_SimpleCalibration();
 	//examples::main_SimpleStructuredLightScanner();
 	//examples::main_SimpleShapeFusion();
-	//examples::main_TimedScans(30.0, 20);
+	//examples::main_TimedScans(30.0, 3);
 	
 	printf("Press any key to exit\n");
 	_getch();  // waits for key
